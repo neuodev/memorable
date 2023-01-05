29e432ffb867cbeeec40c250029ec1d8b75a1579
Ahmed Ibrahim <ib153507@gmail.com>
2023-01-05 14:49:16 +0200

    Add create/get todos routes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+mod todos;
+
+use actix_web::{
+    get, middleware::Logger, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder,
+};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::sync::Mutex;
 
 /**
  * 1) `/` should return data about the server ✅
- * 2) GET /todos
- * 3) POST /todos
+ * 2) GET /todos ✅
+ * 3) POST /todos ✅
  * 4) DLETE /todos
- * 5) POST /todos
+ * 5) PUT /todos
+ * 6) GET /todos/{id}
  */
 
 #[derive(Serialize)]
@@ -26,9 +31,14 @@ struct ErrorRes {
     message: String,
 }
 
+#[derive(Serialize)]
+struct OkResponse {
+    message: String,
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 struct Todo {
-    id: u32,
+    id: usize,
     title: String,
     desc: String,
     is_done: bool,
@@ -69,19 +79,77 @@ async fn create_todo(
     let todos = (*todos_map).entry(addr.clone()).or_insert(Vec::new());
     todos.push(todo.into_inner());
 
-    HttpResponse::Ok().finish()
+    HttpResponse::Ok().json(OkResponse {
+        message: String::from("Todo created successfully"),
+    })
+}
+
+#[get("/todos")]
+async fn get_todos(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
+    let addr = match req.peer_addr() {
+        Some(addr) => addr.ip().to_string(),
+        None => {
+            return HttpResponse::BadRequest().json(ErrorRes {
+                message: String::from("Missing request socket IP address"),
+            })
+        }
+    };
+
+    let mut todos_map = data.todos.lock().unwrap();
+    let todos = (*todos_map).entry(addr.clone()).or_insert(Vec::new());
+
+    HttpResponse::Ok().json(todos)
+}
+
+#[get("/todos/{id}")]
+async fn get_todo_by_id(
+    req: HttpRequest,
+    data: web::Data<AppState>,
+    path: web::Path<usize>,
+) -> impl Responder {
+    let addr = match req.peer_addr() {
+        Some(addr) => addr.ip().to_string(),
+        None => {
+            return HttpResponse::BadRequest().json(ErrorRes {
+                message: String::from("Missing request socket IP address"),
+            })
+        }
+    };
+
+    let mut todos_map = data.todos.lock().unwrap();
+    let todos = (*todos_map).entry(addr).or_insert(Vec::new());
+    let todo_id = path.into_inner();
+    let todo = todos.iter().find(|todo| todo.id == todo_id);
+
+    match todo {
+        Some(todo) => HttpResponse::Ok().json(todo),
+        None => HttpResponse::NotFound().finish(),
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let app_data = web::Data::new(AppState {
+        todos: Mutex::new(HashMap::new()),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(AppState {
-                todos: Mutex::new(HashMap::new()),
-            }))
-            .service(index)
-            .service(create_todo)
+            .wrap(Logger::new(
+                "%a \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %T",
+            ))
+            .app_data(app_data.clone())
+            .service(
+                web::scope("/api/v1")
+                    .service(index)
+                    .service(create_todo)
+                    .service(get_todos)
+                    .service(get_todo_by_id),
+            )
     })
+    .workers(6)
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
